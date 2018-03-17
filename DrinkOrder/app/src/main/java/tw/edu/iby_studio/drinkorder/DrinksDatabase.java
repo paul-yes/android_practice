12bb9e859996b6f3ba2f8b189488751ab4cfb113
yenfang1 <yenfang1@users.noreply.github.com>
2018-03-17 19:11:05 +0800

    new
    New the first practice- DrinkOrder

---
+++ DrinkOrder/app/src/main/java/tw/edu/iby_studio/drinkorder/DrinksDatabase.java
@@ -0,0 +1,10 @@
+package tw.edu.iby_studio.drinkorder;
+
+/**
+ * Created by IBY-Project on 2018/3/17.
+ */
+
+public class DrinksDatabase {
+
+
+}
---
+++ DrinkOrder/app/src/main/java/tw/edu/iby_studio/drinkorder/DrinksMenu.java
@@ -0,0 +1,14 @@
+package tw.edu.iby_studio.drinkorder;
+
+/**
+ * Created by IBY-Project on 2018/3/17.
+ */
+
+public class DrinksMenu {
+
+    public DrinksMenu(){
+
+    }
+
+
+}
